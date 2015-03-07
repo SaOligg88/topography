a56068dea20eb0fa51816e12094d36cd8dba6058
margulies <daniel.margulies@gmail.com>
2015-03-07 20:12:13 +0000

    fixing and adding config files

--- DoFindPaths.m
+++ DoFindPaths.m
@@ -5,11 +5,7 @@ function[clus, countC, randpath] = DoPaths(n)
 
 addpath('./utils');
 %% Load yeo preconfigured:
-thresh=2;
-[surf, surfi, surfm, label] = loadHCPsurf_group('L', 2);
-
-edg = SurfStatEdg(surf);
-lab = [1 2 3 4 6 7 8 9 10 11 13 14 15 16 17]; % skipping 5 and 12
+load config_yeo_L_17rsns.m
 
 %%%%%%%%% Begin %%%%%%%%%
 addpath('./utils/surfstat/*');
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-topography
-==========
+# Topography scripts
+
+## DoFindPaths.m ##
+
+`[clus, countC, randpath] = DoPaths(n);`
+
+where `n` is the number of permutations,
+and a config file is loaded in the script.
+
+## 
+
+
+
