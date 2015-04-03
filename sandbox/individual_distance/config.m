e9675a4cfc0595273b167f868c6746fb08c56b6a
margulies <daniel.margulies@gmail.com>
2015-04-03 06:31:54 +0200

    Delete config.m

--- sandbox/individual_distance/config.m
+++
@@ -1,7 +0,0 @@
-function[thresh, surf, surfi, surfm, label, edg, lab] = config();
-
-thresh=2;
-[surf, surfi, surfm, label] = loadHCPsurf_group('L', 2);
-
-edg = SurfStatEdg(surf);
-lab = [1 2 3 4 6 7 8 9 10 11 13 14 15 16 17]; % skipping 5 and 12
