c7209998408c6b0577614f1e9fa5492685126aa7
SaOligg88 <saoligg88@gmail.com>
2015-06-25 15:10:17 +0200

    added exception handling

--- sandbox/individual_distance/individual_dist_label.py
+++ sandbox/individual_distance/individual_dist_label.py
@@ -10,7 +10,8 @@ from sklearn.utils.arpack import eigsh
 dataDir = '/afs/cbs.mpg.de/projects/mar005_lsd-lemon-surf/probands'
 fsDir = '/afs/cbs.mpg.de/projects/mar004_lsd-lemon-preproc/freesurfer'
 outDir = '/scr/liberia1/temp'
-out_file = '%s/individual_dist_label_20150624.txt' % outDir
+out_file = '%s/individual_dist_label_output_20150625.txt' % outDir
+error_file = '%s/individual_dist_label_error_20150625.txt' % outDir
 fwhm = 10 # for smoothing fiedler vector
 
 
@@ -72,7 +73,7 @@ def runExtrema(data, hemi):
     cml = 'mri_surfcluster --in ./temp.mgz --subject fsaverage5 --hemi %s --thmin %s --annot aparc.a2009s --sum ./temp.log' % (hemi, thmin)
     subprocess.call(cml, shell=True)
     extrema_log = pd.read_csv('./temp.log', skiprows=34, skipinitialspace=21, header=None, dtype={0:np.str})
-    extrema_vertices = [int(extrema_log[0].iloc[i][15:25]) for i in range(len(extrema_log))]
+    extrema_vertices = [int(extrema_log[0].iloc[i][15:24]) for i in range(len(extrema_log))]
     os.remove('./temp.log')
     os.remove('./temp.mgz')
     return extrema_vertices
@@ -80,12 +81,12 @@ def runExtrema(data, hemi):
 
 
 output_dict = {'subject': [], 'dist_A1_parietal': [], 'dist_V1_parietal': [], 'hemisphere': []}
-
+error_dict = {'subject': [], 'hemisphere': []}
 
 for subject in [sub for sub in os.listdir(dataDir) if os.path.isdir(os.path.join(dataDir, sub))]:
     for hemi in ['lh', 'rh']:
 
-        # set file names and location
+        # file names and location
         corr_file = '%s/%s/correlation_maps/%s_lsd_corr_1ab_fsa5_%s.npy' % (dataDir, subject, subject, hemi)
         dist_file = '%s/%s/distance_maps/%s_%s_geoDist_fsa5.mat' % (dataDir, subject, subject, hemi)
         parietal_label_file = '%s/%s/labels/fsa5/%s.G_pariet_inf-Angular_fsa5.label' % (dataDir, subject, hemi)
@@ -95,36 +96,41 @@ for subject in [sub for sub in os.listdir(dataDir) if os.path.isdir(os.path.join
         img2_name = '%s/%s_parietal_peak_%s.png' % (outDir, subject, hemi)
         
 
-        if not False in [os.path.isfile(i) for i in [corr_file, dist_file, parietal_label_file, V1_label_file, A1_label_file]]:
+        if not False in [os.path.isfile(i) for i in [corr_file, dist_file, parietal_label_file, V1_label_file, A1_label_file]]: 
             
-            print '\n\n' + subject
-
-            # read in data
-            cort = np.sort(nib.freesurfer.io.read_label('%s/fsaverage5/label/%s.cortex.label' % (fsDir, hemi)))
-            corr = np.load(corr_file)
-            with h5py.File(dist_file, 'r') as f:
-                    dist = f['dataAll'][()]
-            parietal_vertices = np.sort(nib.freesurfer.io.read_label(parietal_label_file))
-            V1_vertices = np.sort(nib.freesurfer.io.read_label(V1_label_file))
-            A1_vertices = np.sort(nib.freesurfer.io.read_label(A1_label_file))
-
-            # fiedler vector
-            fiedler = np.zeros(len(corr))
-            fiedler[cort] = runFiedler(corr[cort, :][:, cort])[:,0]
-            del corr
-            # smoothing, finding local extrema
-            f_smoothed = runSmoothing(fiedler, hemi, fwhm)
-            f_extrema_vertices = runExtrema(f_smoothed, hemi)
-            img2disc(f_smoothed, foci=f_extrema_vertices, labelfile=parietal_label_file, filename=img1_name, hemi=hemi)
-            # get local peak that is closest to parietal label
-            dist_extrema_2_parietal = [np.mean(dist[parietal_vertices, i]) for i in f_extrema_vertices]
-            parietal_peak_vertex = f_extrema_vertices[dist_extrema_2_parietal.index(min(dist_extrema_2_parietal))]
-            img2disc(f_smoothed, foci=parietal_peak_vertex, filename=img2_name, hemi=hemi)
-
-            # save results
-            output_dict['subject'].append(subject)
-            output_dict['hemisphere'].append(hemi)
-            output_dict['dist_V1_parietal'].append(dist[V1_vertices, parietal_peak_vertex].mean())
-            output_dict['dist_A1_parietal'].append(dist[A1_vertices, parietal_peak_vertex].mean())
+            print '\n\n' + subject          
+            
+            try:
+                # read in data
+                cort = np.sort(nib.freesurfer.io.read_label('%s/fsaverage5/label/%s.cortex.label' % (fsDir, hemi)))
+                corr = np.load(corr_file)
+                with h5py.File(dist_file, 'r') as f:
+                        dist = f['dataAll'][()]
+                parietal_vertices = np.sort(nib.freesurfer.io.read_label(parietal_label_file))
+                V1_vertices = np.sort(nib.freesurfer.io.read_label(V1_label_file))
+                A1_vertices = np.sort(nib.freesurfer.io.read_label(A1_label_file))
+                # fiedler vector
+                fiedler = np.zeros(len(corr))
+                fiedler[cort] = runFiedler(corr[cort, :][:, cort])[:,0]
+                del corr
+                # smoothing, finding local extrema
+                f_smoothed = runSmoothing(fiedler, hemi, fwhm)
+                f_extrema_vertices = runExtrema(f_smoothed, hemi)
+                img2disc(f_smoothed, foci=f_extrema_vertices, labelfile=parietal_label_file, filename=img1_name, hemi=hemi)
+                # get local peak that is closest to parietal label
+                dist_extrema_2_parietal = [np.mean(dist[parietal_vertices, i]) for i in f_extrema_vertices]
+                parietal_peak_vertex = f_extrema_vertices[dist_extrema_2_parietal.index(min(dist_extrema_2_parietal))]
+                img2disc(f_smoothed, foci=parietal_peak_vertex, filename=img2_name, hemi=hemi)
+                # save results
+                output_dict['subject'].append(subject)
+                output_dict['hemisphere'].append(hemi)
+                output_dict['dist_V1_parietal'].append(dist[V1_vertices, parietal_peak_vertex].mean())
+                output_dict['dist_A1_parietal'].append(dist[A1_vertices, parietal_peak_vertex].mean())
+            
+            except:
+                error_dict['subject'].append(subject)
+                error_dict['hemisphere'].append(hemi)
+                pass
             
 pd.DataFrame(output_dict).to_csv(out_file, sep='\t', index=False, columns=['subject', 'hemisphere', 'dist_V1_parietal', 'dist_A1_parietal'])
+pd.DataFrame(error_dict).to_csv(error_file, sep='\t', index=False, columns=['subject', 'hemisphere'])
